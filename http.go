d943426a1c2294b1a912ab746eaf6cba4f1690b7
Felipe Volpatto <felipevolpatto@gmail.com>
2016-07-08 11:24:55 -0300

    Add http.go

---
+++ http.go
@@ -0,0 +1,20 @@
+ckage main
+
+import ( 
+
+  "fmt"
+	"net/http"
+)
+
+
+/* handles http requests for / */
+func handler(w http.ResponseWriter, r *http.Request){
+
+	fmt.Fprintf(w , "Hello world");
+}
+
+func main(){
+
+	http.HandleFunc( "/" , handler )
+	http.ListenAndServe(":8080" , nil)
+}
